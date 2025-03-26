f26c619898624d98cc0a5319d8ed70e66d7baa30
aymane boujeddi <aymane655@gmail.com>
2025-03-26 13:20:03 +0000

    add(UML) : added the use case diagram in the conception folder

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,12 +1,13 @@
 <?php
 
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
